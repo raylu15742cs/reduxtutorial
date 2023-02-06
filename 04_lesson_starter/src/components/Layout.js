125ddbda81b36acc43eb79dd9981959f81b68a5a
raylu15742cs <raylucs15742@gmail.com>
2023-02-06 13:23:45 -0800

    add header

--- 04_lesson_starter/src/components/Layout.js
+++ 04_lesson_starter/src/components/Layout.js
@@ -1,11 +1,15 @@
 import { Outlet } from 'react-router-dom'
+import Header from './Header'
 
 const Layout = () => {
     return (
-        <main className='App'>
-            <Outlet />
+      <>
+        <Header />
+        <main className="App">
+          <Outlet />
         </main>
-    )
+      </>
+    );
 }
 
 export default Layout
