2d572f06f1872c3608289dbcdfa22cd6710b4040
raylu15742cs <raylucs15742@gmail.com>
2023-02-06 13:38:26 -0800

    update

--- 04_lesson_starter/src/features/posts/postsSlice.js
+++ 04_lesson_starter/src/features/posts/postsSlice.js
@@ -20,6 +20,21 @@ export const addNewPost = createAsyncThunk('posts/addNewPost', async (initialPos
     return response.data
 })
 
+export const updatePost = createAsyncThunk(
+  'posts/updatePost',
+  async (initialPost) => {
+    const { id } = initialPost;
+    try {
+      const response = await axios.put(`${POSTS_URL}/${id}`, initialPost);
+      return response.data;
+    } catch (err) {
+      //return err.message;
+      return initialPost; // only for testing Redux!
+    }
+  }
+);
+
+
 const postsSlice = createSlice({
     name: 'posts',
     initialState,
@@ -57,57 +72,68 @@ const postsSlice = createSlice({
     },
     extraReducers(builder) {
         builder
-            .addCase(fetchPosts.pending, (state, action) => {
-                state.status = 'loading'
-            })
-            .addCase(fetchPosts.fulfilled, (state, action) => {
-                state.status = 'succeeded'
-                // Adding date and reactions
-                let min = 1;
-                const loadedPosts = action.payload.map(post => {
-                    post.date = sub(new Date(), { minutes: min++ }).toISOString();
-                    post.reactions = {
-                        thumbsUp: 0,
-                        wow: 0,
-                        heart: 0,
-                        rocket: 0,
-                        coffee: 0
-                    }
-                    return post;
-                });
+          .addCase(fetchPosts.pending, (state, action) => {
+            state.status = 'loading';
+          })
+          .addCase(fetchPosts.fulfilled, (state, action) => {
+            state.status = 'succeeded';
+            // Adding date and reactions
+            let min = 1;
+            const loadedPosts = action.payload.map((post) => {
+              post.date = sub(new Date(), { minutes: min++ }).toISOString();
+              post.reactions = {
+                thumbsUp: 0,
+                wow: 0,
+                heart: 0,
+                rocket: 0,
+                coffee: 0,
+              };
+              return post;
+            });
 
-                // Add any fetched posts to the array
-                state.posts = state.posts.concat(loadedPosts)
-            })
-            .addCase(fetchPosts.rejected, (state, action) => {
-                state.status = 'failed'
-                state.error = action.error.message
-            })
-            .addCase(addNewPost.fulfilled, (state, action) => {
-                // Fix for API post IDs:
-                // Creating sortedPosts & assigning the id 
-                // would be not be needed if the fake API 
-                // returned accurate new post IDs
-                const sortedPosts = state.posts.sort((a, b) => {
-                    if (a.id > b.id) return 1
-                    if (a.id < b.id) return -1
-                    return 0
-                })
-                action.payload.id = sortedPosts[sortedPosts.length - 1].id + 1;
-                // End fix for fake API post IDs 
+            // Add any fetched posts to the array
+            state.posts = state.posts.concat(loadedPosts);
+          })
+          .addCase(fetchPosts.rejected, (state, action) => {
+            state.status = 'failed';
+            state.error = action.error.message;
+          })
+          .addCase(addNewPost.fulfilled, (state, action) => {
+            // Fix for API post IDs:
+            // Creating sortedPosts & assigning the id
+            // would be not be needed if the fake API
+            // returned accurate new post IDs
+            const sortedPosts = state.posts.sort((a, b) => {
+              if (a.id > b.id) return 1;
+              if (a.id < b.id) return -1;
+              return 0;
+            });
+            action.payload.id = sortedPosts[sortedPosts.length - 1].id + 1;
+            // End fix for fake API post IDs
 
-                action.payload.userId = Number(action.payload.userId)
-                action.payload.date = new Date().toISOString();
-                action.payload.reactions = {
-                  thumbsUp: 0,
-                  wow: 0,
-                  heart: 0,
-                  rocket: 0,
-                  coffee: 0,
-                };
-                console.log(action.payload)
-                state.posts.push(action.payload)
-            })
+            action.payload.userId = Number(action.payload.userId);
+            action.payload.date = new Date().toISOString();
+            action.payload.reactions = {
+              thumbsUp: 0,
+              wow: 0,
+              heart: 0,
+              rocket: 0,
+              coffee: 0,
+            };
+            console.log(action.payload);
+            state.posts.push(action.payload);
+          })
+          .addCase(updatePost.fulfilled, (state, action) => {
+            if (!action.payload?.id) {
+              console.log('Update could not complete');
+              console.log(action.payload);
+              return;
+            }
+            const { id } = action.payload;
+            action.payload.date = new Date().toISOString();
+            const posts = state.posts.filter((post) => post.id !== id);
+            state.posts = [...posts, action.payload];
+          });
     }
 })
 
