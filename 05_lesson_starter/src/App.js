c93767181c62d23b63234af29979df98033a26eb
raylu15742cs <raylucs15742@gmail.com>
2023-02-08 15:05:12 -0800

    404 request

--- 05_lesson_starter/src/App.js
+++ 05_lesson_starter/src/App.js
@@ -2,8 +2,11 @@ import PostsList from "./features/posts/PostsList";
 import AddPostForm from "./features/posts/AddPostForm";
 import SinglePostPage from "./features/posts/SinglePostPage";
 import EditPostForm from "./features/posts/EditPostForm";
+import UsersList from "./features/users/UsersLists";
+import UserPage from "./features/users/UserPage";
+
 import Layout from "./components/Layout";
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
   return (
@@ -17,6 +20,14 @@ function App() {
           <Route path=":postId" element={<SinglePostPage />} />
           <Route path="edit/:postId" element={<EditPostForm />} />
         </Route>
+
+        <Route path="user">
+          <Route index element={<UsersList />} />
+          <Route path="userId" elemeent={<UserPage />} />      
+        </Route>
+
+        {/* Catch all = replace with 404 component if you want */}
+        <Route path="*" element={<Navigate to="/" replace />} />
 
       </Route>
     </Routes>
