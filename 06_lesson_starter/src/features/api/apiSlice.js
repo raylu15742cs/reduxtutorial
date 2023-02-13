b01d3e16eb190a038d6e92538d31e353d38035fa
raylu15742cs <raylucs15742@gmail.com>
2023-02-13 11:57:17 -0800

    update

--- 06_lesson_starter/src/features/api/apiSlice.js
+++ 06_lesson_starter/src/features/api/apiSlice.js
@@ -1,35 +1,40 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const apiSlice = createApi({
-    reducerPath: 'api',
-    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3500'}),
-    endpoints: (builder) => ({
-        getTodos: builder.query({
-            query: () => '/todos',
-        }),
-        addTodo: builder.mutation({
-            query: (todo) => ({
-                url: '/todos',
-                method: 'POST',
-                body: todo
-            })
-        }),
-        updateTodo: builder.mutation({
-            query: (todo) => ({
-                url: `/todos/${todo.id}`,
-                method: 'PATCH',
-                body: todo
-            })
-        }),
-        deleteTodo: builder.mutation({
-            query: ({ id }) => ({
-                url: `/todos/${id}`,
-                method: 'DELETE',
-                body: id
-            })
-        })
-    }) 
-})
+  reducerPath: 'api',
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3500' }),
+  tagTypes: ['Todos'],
+  endpoints: (builder) => ({
+    getTodos: builder.query({
+      query: () => '/todos',
+      providesTags: ['Todos'],
+    }),
+    addTodo: builder.mutation({
+      query: (todo) => ({
+        url: '/todos',
+        method: 'POST',
+        body: todo,
+      }),
+      invalidatesTags: ['Todos'],
+    }),
+    updateTodo: builder.mutation({
+      query: (todo) => ({
+        url: `/todos/${todo.id}`,
+        method: 'PATCH',
+        body: todo,
+      }),
+      invalidatesTags: ['Todos'],
+    }),
+    deleteTodo: builder.mutation({
+      query: ({ id }) => ({
+        url: `/todos/${id}`,
+        method: 'DELETE',
+        body: id,
+      }),
+      invalidatesTags: ['Todos'],
+    }),
+  }),
+});
 
 export const {
     useGetTodosQuery,
