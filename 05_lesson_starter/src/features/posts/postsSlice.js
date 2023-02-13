36d1e46e8738b72b4e3702f844c2e2c2e5926eab
raylu15742cs <raylucs15742@gmail.com>
2023-02-13 10:06:23 -0800

    update selectors

--- 05_lesson_starter/src/features/posts/postsSlice.js
+++ 05_lesson_starter/src/features/posts/postsSlice.js
@@ -134,14 +134,17 @@ const postsSlice = createSlice({
             })
     }
 })
+// getSelectors creates these selector and we rename them with aliases using destructuring
+export const {
+    selectAll : selectAllPosts,
+    selectById: selectPostById,
+    selectIds: selectPostIds,
+} = postsAdapter.getSelectors(state => state.posts)
 
-export const selectAllPosts = (state) => state.posts.posts;
 export const getPostsStatus = (state) => state.posts.status;
 export const getPostsError = (state) => state.posts.error;
 export const getCount = (state) => state.posts.count;
 
-export const selectPostById = (state, postId) =>
-    state.posts.posts.find(post => post.id === postId);
 
 export const selectPostsByUser = createSelector(
     [selectAllPosts , (state, userId) => userId],
