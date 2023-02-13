f98afc62868a2ed2a41bb0c8e5625efecbb6f180
raylu15742cs <raylucs15742@gmail.com>
2023-02-13 11:46:23 -0800

    update apislice

--- 06_lesson_starter/src/features/api/apiSlice.js
+++ 06_lesson_starter/src/features/api/apiSlice.js
@@ -6,10 +6,34 @@ export const apiSlice = createApi({
     endpoints: (builder) => ({
         getTodos: builder.query({
             query: () => '/todos',
+        }),
+        addTodo: builder.mutation({
+            query: (todo) => ({
+                url: '/todos',
+                method: 'POST',
+                body: todo
+            })
+        }),
+        updateTodo: builder.mutation({
+            query: (todo) => ({
+                url: `/todos/${todo.id}`,
+                method: 'PATCH',
+                body: todo
+            })
+        }),
+        deleteTodo: builder.mutation({
+            query: ({ id }) => ({
+                url: `/todos/${id}`,
+                method: 'DELETE',
+                body: id
+            })
         })
     }) 
 })
 
 export const {
-    useGetTodosQuery
+    useGetTodosQuery,
+    useAddTodoMutation,
+    useUpdateTodoMutation,
+    useDeleteTodoMutation
 } = apiSlice
